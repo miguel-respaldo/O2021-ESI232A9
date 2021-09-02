82306539b2a7a9f1ef8ef8dce4a3755f16ace44c
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-09-02 08:22:36 -0500

    suma int y float

---
+++ 0902_hola_mundo/suma_float.py
@@ -0,0 +1,6 @@
+num1 = float(input("Escribe el primer numero: "))
+num2 = float(input("Escribe el segundo numero: "))
+
+suma = num1 + num2
+
+print("El resultado de la suma es ", suma)
---
+++ 0902_hola_mundo/suma_int.py
@@ -0,0 +1,6 @@
+num1 = int(input("Escribe el primer numero: "))
+num2 = int(input("Escribe el segundo numero: "))
+
+suma = num1 + num2
+
+print("El resultado de la suma es ", suma)
