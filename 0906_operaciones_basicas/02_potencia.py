c597fbcaf1a7401be7e0a4ad2144500ce78e3bc7
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-06 07:56:52 -0500

    potencia

---
+++ 0906_operaciones_basicas/02_potencia.py
@@ -0,0 +1,10 @@
+import math
+# Leemos la entrada de datos
+numero = eval(input("Escribe un numero: "))
+
+# Hacemos la operacion de potencia
+cuadrado = numero**2
+
+# Mostramos resultado
+print("El cuadrado es", cuadrado)
+
