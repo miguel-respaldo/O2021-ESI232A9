27c3cf0559def27d5f5fc2daed44ebdc7b64f068
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-06 07:46:54 -0500

    raiz cuadrada 2

--- 0906_operaciones_basicas/01_raiz_cuadrada.py
+++ 0906_operaciones_basicas/01_raiz_cuadrada.py
@@ -1,2 +1,10 @@
 import math
+# Leemos la entrada de datos
+numero = eval(input("Escribe un numero: "))
+
+# Hacemos la operacion de raiz cuadrada
+raiz = math.sqrt(numero)
+
+# Mostramos resultado
+print("La raiz cuadrada de", numero, " es", raiz)
 
