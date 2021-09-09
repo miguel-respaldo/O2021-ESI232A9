9598afd5c68ebc489d5092d4fb4f929f285dee15
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-09 08:37:31 -0500

    mas funciones

---
+++ 0909-cadenas_de_texto/05-mas-funciones-de-cadena.py
@@ -0,0 +1,6 @@
+s = 'Bienvenido a Python'
+print("Termina con thon:",s.endswith("thon"))
+print("Comienza con Buen:",s.startswith("Buen"))
+print("Encuentra la posición ido:",s.find("ido"))
+print("Encuentra por derecha la posición de venida:",s.rfind("venida"))
+print("Cuantas 'o' hay en el texto:",s.count("o"))
