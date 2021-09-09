9623fe8878d559d4645f5c8c0912c84eab956b3a
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-09 08:23:45 -0500

    funciones de texto

---
+++ 0909-cadenas_de_texto/04-funciones_de_cadenas.py
@@ -0,0 +1,6 @@
+s1 = "Hola Mundo"
+
+print("Es digito:",s.isdigit())
+print("Es del alfabeto:", s.isalpha())
+print("Esta todo en mayusculas:", s.isupper())
+print("Esta todo en minusculas:", s.islower())
