3fc4959228e06e0f5fdfdb98138c8d68c2c6b9e8
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-09 07:36:18 -0500

    cadena de texto indices

--- 0909-cadenas_de_texto/01-cadenas_de_texto.py
+++ 0909-cadenas_de_texto/01-cadenas_de_texto.py
@@ -9,3 +9,11 @@ print("La cadena es:",s3)
 
 s4 = str("Libreta")
 print("La cadena es:",s4)
+
+print("--------------------")
+#  Libreta
+#  0123456
+
+print("La primera letra:",s4[0])
+print("La segunda letra:",s4[1])
+print("La última letra:",s4[6])
