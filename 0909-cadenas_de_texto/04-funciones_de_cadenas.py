d792f7f4fcf8b96eae9e6a06c63132e13ea1e541
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-09 08:26:45 -0500

    funciones de texto 3

--- 0909-cadenas_de_texto/04-funciones_de_cadenas.py
+++ 0909-cadenas_de_texto/04-funciones_de_cadenas.py
@@ -1,6 +1,40 @@
 s1 = "Hola Mundo"
-
+print("La cadena es:", s1)
 print("Es digito:",s1.isdigit())
 print("Es del alfabeto:", s1.isalpha())
 print("Esta todo en mayusculas:", s1.isupper())
 print("Esta todo en minusculas:", s1.islower())
+print("----------------------")
+
+s1 = "5873"
+print("La cadena es:", s1)
+print("Es digito:",s1.isdigit())
+print("Es del alfabeto:", s1.isalpha())
+print("Esta todo en mayusculas:", s1.isupper())
+print("Esta todo en minusculas:", s1.islower())
+print("----------------------")
+
+s1 = "hola mundo"
+print("La cadena es:", s1)
+print("Es digito:",s1.isdigit())
+print("Es del alfabeto:", s1.isalpha())
+print("Esta todo en mayusculas:", s1.isupper())
+print("Esta todo en minusculas:", s1.islower())
+print("----------------------")
+
+s1 = "holamundo"
+print("La cadena es:", s1)
+print("Es digito:",s1.isdigit())
+print("Es del alfabeto:", s1.isalpha())
+print("Esta todo en mayusculas:", s1.isupper())
+print("Esta todo en minusculas:", s1.islower())
+print("----------------------")
+
+s1 = "HOLAMUNDO"
+print("La cadena es:", s1)
+print("Es digito:",s1.isdigit())
+print("Es del alfabeto:", s1.isalpha())
+print("Esta todo en mayusculas:", s1.isupper())
+print("Esta todo en minusculas:", s1.islower())
+print("----------------------")
+
