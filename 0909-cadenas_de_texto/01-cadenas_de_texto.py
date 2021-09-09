035fda1ea877b19cd77afd07a87ceee6e4cf8b80
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-09 07:31:49 -0500

    cadena de texto

---
+++ 0909-cadenas_de_texto/01-cadenas_de_texto.py
@@ -0,0 +1,11 @@
+s1 = str() # Nos crea una cadena vacia de texto
+print("La cadena es:",s1)
+
+s2 = ""
+print("La cadena es:",s2)
+
+s3 = "Libreta"
+print("La cadena es:",s3)
+
+s4 = str("Libreta")
+print("La cadena es:",s4)
