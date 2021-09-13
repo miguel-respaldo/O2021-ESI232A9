76e9b3d7a4747195feba0683e8021257d601bf69
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-13 08:33:01 -0500

    ejercicio contador de palabras

---
+++ tareas/contador_vocales.py
@@ -0,0 +1,8 @@
+palabra = input("Escribe una palabra y te contare las vocales:")
+
+print("Hay",palabra.count("a"),"a")
+print("Hay",palabra.count("e"),"e")
+print("Hay",palabra.count("i"),"i")
+print("Hay",palabra.count("o"),"o")
+print("Hay",palabra.count("u"),"u")
+
