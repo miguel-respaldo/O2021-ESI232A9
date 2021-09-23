db66991fc26687a6566b8e0875ab9e9a37ae1732
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-23 08:42:37 -0500

    multiple mayor dos numeros

---
+++ 0923-sentencias_selectivas/10-mayor_de_dos_numeros_multiple.py
@@ -0,0 +1,9 @@
+num1 = input("Escribe el primer número: ")
+num2 = input("Escribe el segundo número: ")
+
+if num1 > num2:
+    print("El número", num1, "es mayor")
+elif num2 > num1:
+    print("El número", num2, "es mayor")
+else:
+    print("Los numeros son iguales")
