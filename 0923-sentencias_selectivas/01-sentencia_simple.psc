884dfdf6014e17be61804f6092f4afa95043f3ea
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-09-23 07:37:22 -0500

    sentencias selectivas

---
+++ 0923-sentencias_selectivas/01-sentencia_simple.psc
@@ -0,0 +1,10 @@
+Algoritmo sentencia_simple
+	Escribir "Ingresa tu edad:"
+	Leer edad
+	
+	Si edad >= 18 Entonces
+		Escribir "Ya eres mayor de edad"
+	Fin Si
+	
+	Escribir "Gracias por la informacion"
+FinAlgoritmo
---
+++ 0923-sentencias_selectivas/02-sentencias_simple.py
@@ -0,0 +1,15 @@
+#Escribir "Ingresa tu edad:"
+#Leer edad
+
+#Si edad >= 18 Entonces
+#  Escribir "Ya eres mayor de edad"
+#FinSi
+
+#Escribir "Gracias por la informacion"
+
+edad = eval(input("Ingresa tu edad: "))
+
+if edad >= 18:
+    print("Ya eres mayor de edad")
+
+print("Gracias por la información")
