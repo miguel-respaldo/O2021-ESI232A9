c9a758afcbcc3ae3bc55c39098a4a6b43eb25c9d
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-04 07:50:32 -0500

    ejemplo for 4

---
+++ 1004-sentencias_repetitivas_for/05-ejemplo_continue.py
@@ -0,0 +1,5 @@
+frutas = ["manzana", "platano", "cereza"]
+for x in frutas:
+    if x == "platano":
+        continue
+    print(x)
---
+++ 1004-sentencias_repetitivas_for/07-for.py
@@ -0,0 +1,5 @@
+for x in range(6):
+    print(x)
+
+for x in range(3):
+    print("hola")
