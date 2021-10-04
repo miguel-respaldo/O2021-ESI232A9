98f977af372d6a1a1a4c413e9293d88c3e8b3e02
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-04 08:44:38 -0500

    for else

---
+++ 1004-sentencias_repetitivas_for/13_anidados.py
@@ -0,0 +1,8 @@
+adjetivo = ["roja", "grande", "dulce"]
+frutas = ["manzana", "platano", "naranja"]
+
+for x in adjetivo:
+    print("Adjetivo INICIA", x)
+    for y in frutas:
+        print(y, x)
+    print("Adjetivo FIN", x)
