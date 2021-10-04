a386e669b3e08ce21af26bad0308e98805d4558d
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-04 07:41:52 -0500

    ejemplo for 3

---
+++ 1004-sentencias_repetitivas_for/03-ejemplo_break.py
@@ -0,0 +1,5 @@
+frutas = ["manzana", "platano", "cereza"]
+for x in frutas:
+    print(x)
+    if x == "platano":
+        break
---
+++ 1004-sentencias_repetitivas_for/04-ejemplo_break2.py
@@ -0,0 +1,5 @@
+frutas = ["manzana", "platano", "cereza"]
+for x in frutas:
+    if x == "platano":
+        break
+    print(x)
