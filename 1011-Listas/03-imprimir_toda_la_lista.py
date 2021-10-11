5c1de35726fc1e09d9f07905e2dcde98a7cd2412
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 07:40:24 -0500

    listas 5

---
+++ 1011-Listas/03-imprimir_toda_la_lista.py
@@ -0,0 +1,6 @@
+una_lista = ["manzana", "platano", "naranja", "pera", "manzana"]
+#               0          1           2        3        4
+num_elementos = len(una_lista)
+
+for elemento in range(num_elementos):
+    print(una_lista[elemento])
