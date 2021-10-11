f0f9e8a780c17329eecce8f9cde4c2dd24e14182
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 07:51:56 -0500

    listas 8

---
+++ 1011-Listas/05-rango.py
@@ -0,0 +1,3 @@
+una_lista = ["manzana", "platano", "naranja", "pera", "kiwi", "mango", "melon"]
+#               0           1         2         3       4        5        6
+print("Desde el 2 hasta el 5 (sin incluirlo):", una_lista[2:5])
