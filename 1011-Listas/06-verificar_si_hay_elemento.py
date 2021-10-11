2407f04e7df344e58a02faf03c95e3acbfdd2170
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-11 08:10:25 -0500

    listas 9

---
+++ 1011-Listas/06-verificar_si_hay_elemento.py
@@ -0,0 +1,10 @@
+una_lista = ["manzana", "platano", "naranja", "pera", "kiwi", "mango", "melon"]
+#               0           1         2         3       4        5        6
+#              -7           -6        -5        -4      -3      -2       -1
+
+if "manzana" in una_lista:
+    print("Si, la manzana esta en la lista")
+
+
+if "sandia" not in una_lista:
+    print("La sandia NO esta en la lista")
