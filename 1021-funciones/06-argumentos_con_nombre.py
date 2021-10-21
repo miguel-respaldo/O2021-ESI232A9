043aec1819ddda00dd43c86903298d30fcb93e6c
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-10-21 07:53:52 -0500

    funciones suma 2

---
+++ 1021-funciones/06-argumentos_con_nombre.py
@@ -0,0 +1,5 @@
+def el_mayor(num1, num2, num3):
+    print("El mayor es", num3)
+
+el_mayor(1,2,3)
+el_mayor(num3=5, num2=4, num1=2)
---
+++ 1021-funciones/07-return.py
@@ -0,0 +1,9 @@
+def suma(num1, num2):
+    return num1 + num2
+
+numero1 = eval(input("Escribe un número: "))
+numero2 = eval(input("Escribe otro número: "))
+
+resultado = suma(numero1, numero2)
+
+print("El resutlado es:", resultado)
